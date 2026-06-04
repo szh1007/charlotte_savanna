ccc3da6cdcbb3061c22e5eb95bf18488400d1a86
charlotte <gw1007charlotte@gmail.com>
2026-06-04 15:21:24 +0800

    feat: LangChain Model_IO Ollama

---
+++ LangChain/1_Model_IO/4_调用本地模型_Ollama.py
@@ -0,0 +1,10 @@
+""" 调用本地大模型 Ollama """
+
+"""
+1.下载 Ollama: https://ollama.com/download
+2.在 Ollama 中下载模型 Models, 例如 ollama run deepseek-r1:7b
+"""
+from langchain_ollama import ChatOllama
+
+model = ChatOllama(model="deepseek-r1:7b")
+print(model.invoke("介绍一下Ollama").content)
