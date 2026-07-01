6e4413c94223ed8a13971f11893d953adc9c93c3
charlotte <gw1007charlotte@gmail.com>
2026-07-02 01:21:32 +0800

    feat: LangChain RAG 1

---
+++ LangChain/6_RAG/1_loader.py
@@ -0,0 +1,58 @@
+""" 文档加载器 loader """
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from langchain_community.document_loaders import (
+    TextLoader,
+    PyPDFLoader,
+    CSVLoader,
+    JSONLoader,
+)
+
+""" txt """
+txt1 = TextLoader("./asset/01-langchain-gbk.txt").load()
+txt2 = TextLoader(file_path="./asset/01-langchain-utf-8.txt", encoding="utf-8").load()
+# for i, line in enumerate(txt1 + txt2):
+#     print(i + 1, line.metadata, "\n", line.page_content)
+
+""" pdf """
+pdf1 = PyPDFLoader("./asset/02-load.pdf").load()  # 本地pdf文件
+pdf2 = PyPDFLoader("https://arxiv.org/pdf/2302.03803").load()  # 网上pdf文件
+# for i, page in enumerate(pdf1 + pdf2):
+#     print(i + 1, page.metadata, "\n", page.page_content)
+
+""" csv """
+csv = CSVLoader(file_path="./asset/03-load.csv").load()
+# for i, line in enumerate(csv):
+#     print(i + 1, line.metadata, "\n", line.page_content)
+
+""" json """
+json1 = JSONLoader(
+    file_path="./asset/04-load.json",
+    jq_schema=".",  # 加载所有数据
+    text_content=False,  # 把数据转换为string
+).load()
+# print(json1)
+
+json2 = JSONLoader(
+    file_path="./asset/04-load.json",
+    jq_schema=".messages[].content",  # 加载messages中的content字段
+).load()
+# for item in json2:
+#     print(item.page_content)
+
+json3 = JSONLoader(
+    file_path="./asset/04-response.json",
+    jq_schema=".data.items[].content",
+).load()
+# for item in json3:
+#     print(item.page_content)
+
+json4 = JSONLoader(
+    file_path="./asset/04-response.json",
+    jq_schema=".data.items[]",
+    content_key='.title + "\n" +.content',
+    is_content_key_jq_parsable=True,
+).load()
+# for item in json4:
+#     print(item.page_content)
---
+++ LangChain/6_RAG/2_splitter.py
@@ -0,0 +1,102 @@
+""" 文档拆分器 splitter """
+import os, dotenv, warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+    TokenTextSplitter,
+)
+
+dotenv.load_dotenv()
+os.environ["OPENAI_BASE_URL"] = os.getenv("OPENAI_BASE_URL", "")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY", "")
+
+test_str = """
+LangChain 是一个用于构建基于大语言模型应用的开发框架。
+
+旨在帮助开发者更高效地集成、管理和增强大语言模型的能力，构建端到端的应用程序。
+它提供了一套模块化工具和接口，支持从简单的文本生成到复杂的多步骤推理任务。
+"""
+
+""" 字符切分器 CharacterTextSplitter """
+splitter1 = CharacterTextSplitter(
+    chunk_size=50,  # 分块大小
+    chunk_overlap=0,    # 块之间的重叠大小
+    separator="。",   # 优先使用该分隔符切分, 为空时表示禁用分隔符优先
+)
+
+# texts1 = splitter1.split_text(test_str)
+# for i, chunk in enumerate(texts1):
+#     print(f"[{i + 1}]: {len(chunk)}")
+#     print(chunk)
+#     print("-" * 50)
+
+""" 递归字符拆分器 RecursiveCharacterTextSplitter """
+splitter2 = RecursiveCharacterTextSplitter(
+    chunk_size=30,
+    chunk_overlap=0,
+    add_start_index=True,
+    # separator 默认 ["\n\n", "\n", " ", ""]
+)
+
+# texts2 = splitter2.create_documents([test_str])
+# for i, chunk in enumerate(texts2):
+#     print(chunk)
+#     print("-" * 50)
+
+
+with open("./asset/08-ai1.txt", "r", encoding="utf-8") as f:
+    txt_str = f.read()
+
+splitter3 = RecursiveCharacterTextSplitter(
+    chunk_size=50,
+    chunk_overlap=20,
+    add_start_index=True,
+    length_function=len,
+    separators=["。", "，"],
+)
+
+# texts3 = splitter3.create_documents([txt_str])
+# for i, chunk in enumerate(texts3):
+#     print(chunk.page_content)
+#     print("-" * 50)
+
+PDF = PyPDFLoader("./asset/02-load.pdf").load()
+# docs = splitter3.split_documents(PDF)
+# for i, chunk in enumerate(docs):
+#     print(chunk.page_content)
+#     print("-" * 50)
+
+""" token拆分器 TokenTextSplitter """
+splitter4 = TokenTextSplitter(
+    chunk_size=50,
+    chunk_overlap=0,
+    encoding_name="cl100k_base",
+)
+
+# texts4 = splitter4.split_text(test_str)
+# for i, chunk in enumerate(texts4):
+#     print(f"[{i + 1}]: {len(chunk)}")
+#     print(chunk)
+#     print("-" * 50)
+
+
+""" 语义拆分器 SemanticChunker """
+embed = OpenAIEmbeddings(model="text-embedding-3-large")
+splitter5 = SemanticChunker(
+    embeddings=embed,
+    breakpoint_threshold_type="percentile",
+    breakpoint_threshold_amount=65.0,
+)
+with open("./asset/09-ai2.txt", "r", encoding="utf-8") as f:
+    ai_str = f.read()
+
+texts5 = splitter5.create_documents([ai_str])
+# for i, chunk in enumerate(texts5):
+#     print(chunk)
+#     print("-" * 50)
+# print(len(texts5))
