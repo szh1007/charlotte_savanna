6ed9dad2eaa409c4f2e27456099777ef2ab6c955
charlotte <gw1007charlotte@gmail.com>
2026-07-08 18:24:23 +0800

    feat: update mysql settings

--- charlotte_savanna/settings.py
+++ charlotte_savanna/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-import os
+import os, pymysql
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +85,12 @@ WSGI_APPLICATION = 'charlotte_savanna.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'charlotte'),
+        'USER': os.environ.get('DB_USERNAME', 'root'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
