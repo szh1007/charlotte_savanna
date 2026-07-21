92a93b8d654d4b7f5cdeffa299a85db6077a5e93
charlotte <gw1007charlotte@gmail.com>
2026-07-21 21:52:54 +0800

    feat: memory 1

---
+++ demo/LangChain_20260714/_9_memory/9_1_memory_short_term_checkpointer.py
@@ -0,0 +1,44 @@
+import dotenv
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
+from rich import print as rprint
+
+dotenv.load_dotenv()
+
+model = init_chat_model("deepseek:deepseek-v4-pro", extra_body={"thinking": {"type": "disabled"}})
+agent = create_agent(
+    model=model,
+    checkpointer=InMemorySaver(),  # *内存级记忆存储*
+)
+
+# *配置会话线程*
+config = {"configurable": {"thread_id": "1"}}
+
+# 第1轮对话
+messages1 = [
+    ("system", "你是专业的智能AI助手"),
+    ("human", "你好, 我叫Charlotte"),
+    ("ai", "你好, 有什么可以帮到你"),
+    ("human", "5! =?"),
+    ("ai", "5! = 120"),
+    ("human", "10! = ?"),
+]
+response = agent.invoke({"messages": messages1}, config=config)
+rprint(response["messages"][-1].content)
+rprint("-" * 50)
+
+# 查看内存记忆
+rprint(agent.get_state(config=config))
+rprint("-" * 50)
+
+# 第2轮对话
+messages2 = [
+    ("human", "你还记得我叫什么吗"),
+]
+response = agent.invoke({"messages": messages2}, config=config)
+rprint(response["messages"][-1].content)
+rprint("-" * 50)
+
+# 查看内存记忆
+rprint(agent.get_state(config=config))
