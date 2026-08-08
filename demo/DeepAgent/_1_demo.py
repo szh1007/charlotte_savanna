d42774eec319a3f3869a52490e957d1068846a3a
charlotte <gw1007charlotte@gmail.com>
2026-08-09 01:05:40 +0800

    feat: deepagents subagent

---
+++ demo/DeepAgent/_1_demo.py
@@ -0,0 +1,70 @@
+import dotenv
+from deepagents import create_deep_agent
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+from rich import print as rprint
+from tavily import TavilyClient
+
+"""
+DeepAgent 3条铁律
+1.问题极度开放: 已经不足以用 LangGraph 条件边解决
+2.存在领域冲突: 单智能体执行的话, 不同领域之间会互相污染上下文, 降低大模型能力
+    主智能体和子智能体有【不同的上下文】
+3.需要多方向并行: 不同类型的任务, 并行可以提高效率
+4.需要使用多类型的模型: 语言模型 / 视觉模型 / 嵌入式模型 / ...
+
+DeepAgent 架构模式 - 层级工作流 - 主从模式
+1.主agent负责任务调度和结果反馈, 子agent专注某个方向的任务
+2.主agent挂了, 所有子agent就无法运作了
+3.子agent之间的所有内容(上下文、运行时、记忆等)相互【隔离】
+"""
+
+dotenv.load_dotenv()
+
+model = init_chat_model(
+    "deepseek:deepseek-v4-pro", extra_body={"thinking": {"type": "disabled"}}
+)
+
+tavily = TavilyClient()
+
+
+@tool
+def network_search(query: str, max_results: int = 3) -> str:
+    """
+    联网搜索工具
+
+    Args:
+        query: 搜索查询
+        max_results: 最大返回结果数, 默认3
+
+    Returns:
+        搜索结果
+    """
+    rprint(f"开始联网搜索: {query}")
+    rprint("-" * 100)
+    return tavily.search(query, max_results=max_results)
+
+
+agent = create_deep_agent(
+    model=model,
+    tools=[network_search],
+    system_prompt="""
+        角色: 专家级的研究员
+        边界: 你有权使用联网搜索工具收集信息
+        功能: 深入研究并撰写一份精美的研究报告
+    """,
+)
+
+# result = agent.invoke({"messages": [("human", "米其林餐厅评级的来源")]})
+# rprint(result["messages"][-1].content)
+
+for chunk in agent.stream({"messages": [("human", "米其林餐厅评级的来源")]}):
+    # model -- 分析 --> tool_calls / subagent / output -> AIMessage
+    # tools -- 执行 --> ToolMessage
+    if chunk.get("model"):
+        message = chunk["model"]["messages"][-1]
+        tool_calls = message.tool_calls
+
+        rprint(message.content) if message.content else ""
+        rprint(tool_calls) if tool_calls else ""
+        rprint("-" * 100)
---
+++ demo/DeepAgent/_2_1_subagent.py
@@ -0,0 +1,106 @@
+import dotenv
+from deepagents import create_deep_agent
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+from rich import print as rprint
+from tavily import TavilyClient
+
+"""
+多智能体配置
+
+1.CompiledSubAgent - 不常用、强耦合 - 主要用于兼容 LangChain / LangGraph
+2.dict - 主流推荐
+    name
+    description
+    system_prompt
+    mode - 默认不配置, 则使用主agent的同型号模型
+    tools
+"""
+
+dotenv.load_dotenv()
+
+tavily = TavilyClient()
+
+model = init_chat_model(
+    "deepseek:deepseek-v4-pro", extra_body={"thinking": {"type": "disabled"}}
+)
+
+
+@tool
+def search_weather(query: str, max_results: int = 3) -> str:
+    """
+    联网搜索天气的工具
+
+    Args:
+        query: 查询语句
+        max_results: 最大返回结果数, 默认3
+
+    Returns:
+        天气搜索结果
+    """
+    rprint(f"开始联网搜索天气: {query}")
+    rprint("-" * 100)
+    return tavily.search(query, max_results=max_results)
+
+
+math_sub_agent = {
+    "name": "高级数学计算助手",
+    "description": "能够计算非常复杂的数学表达式, 包含高等数学等专业性极强的任务",
+    "system_prompt": "你是一个专业的数学计算助手, 能够计算非常复杂的数学表达式",
+    "tools": [],
+}
+
+
+translate_sub_agent = {
+    "name": "翻译助手",
+    "description": "能够根据用户需求翻译文本",
+    "system_prompt": "你是专业的翻译助手, 可以根据用户需求使用文本翻译",
+    "tools": [],
+}
+
+# # CompiledSubAgent 集成 LangChain
+# # 集成 LangGraph 同理, runnable=graph 即可
+# langchain_agent = create_agent(
+#     model=model,
+#     tools=[search_weather],
+# )
+# weather_sub_agent = CompiledSubAgent(
+#     name="天气查询助手",
+#     description="能够根据用户需求查询天气的详细信息",
+#     runnable=langchain_agent,
+# )
+
+weather_sub_agent = {
+    "name": "天气查询助手",
+    "description": "能够根据用户需求查询天气的详细信息",
+    "system_prompt": "你是专业的天气查询助手, 可以根据用户需求使用工具查询天气",
+    "tools": [search_weather],
+}
+
+
+agent = create_deep_agent(
+    model=model,
+    subagents=[math_sub_agent, weather_sub_agent, translate_sub_agent],
+    system_prompt="""
+        角色: 综合助手
+        功能:
+            math_sub_agent - 数学计算
+            weather_sub_agent - 天气查询
+            translate_sub_agent - 文本翻译
+        边界: 作为主智能体, 只负责分析和任务分配, 不执行任务, 任务交给子智能体
+    """,
+)
+
+
+if __name__ == "__main__":
+    query = "查一下深圳2026年8月9日的天气, \
+        并算一下2026年8月9日之前7天的平均最高温, \
+            最终将结果翻译成英文展示"
+
+    for chunk in agent.stream({"messages": [("human", query)]}):
+        if chunk.get("model"):
+            message = chunk["model"]["messages"][-1]
+            rprint(message.content) if message.content else ""
+            rprint("-" * 100)
+        else:
+            rprint(chunk)
