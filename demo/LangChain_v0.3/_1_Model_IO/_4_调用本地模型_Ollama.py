d43e29f12079bae5f90f97fcda522bb72a5c4a66
charlotte <gw1007charlotte@gmail.com>
2026-08-17 16:16:31 +0800

    fix: delete demo LangChain_v0.3

--- demo/LangChain_v0.3/_1_Model_IO/_4_调用本地模型_Ollama.py
+++
@@ -1,9 +0,0 @@
-"""调用本地大模型 Ollama
-
-1.下载 Ollama: https://ollama.com/download
-2.在 Ollama 中下载模型 Models, 例如 ollama run deepseek-r1:7b
-"""
-
-from langchain_ollama import ChatOllama
-
-model = ChatOllama(model="deepseek-r1:7b")
