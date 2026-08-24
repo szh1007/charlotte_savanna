42ed4c85a0b946e8e17eb875eb850fa8d70717d1
charlotte <gw1007charlotte@gmail.com>
2026-08-24 21:16:23 +0800

    feat(rag_knowledge): load_graph node_01

--- project/rag_knowledge/app/process/load/nodes/_01_entry.py
+++ project/rag_knowledge/app/process/load/nodes/_01_entry.py
@@ -1,13 +1,25 @@
+from rich import print as rprint
+
 from ....rag.load.entry_service import resolve_input_file
 from ....shared.runtime.logger import node_log
 from ....shared.utils.task_utils import add_done_task, add_running_task
-from ..agent.state import LoadState
+from ..agent.state import LoadState, create_default_state
 
 
 @node_log("node_entry")
 def node_entry(state: LoadState) -> LoadState:
-    """入口节点"""
+    """
+    入口节点
+    识别输入的文件类型, 修改 state 的对应状态
+    """
     add_running_task(state["task_id"], "node_entry")
     state = resolve_input_file(state)
     add_done_task(state["task_id"], "node_entry")
     return state
+
+
+if __name__ == "__main__":
+    # rprint(node_entry(create_default_state(task_id="1", local_file_path="test.txt")))
+
+    test_pdf = "./project/rag_knowledge/assets/hl3070使用说明书.pdf"
+    rprint(node_entry(create_default_state(task_id="1", local_file_path=test_pdf)))
--- project/rag_knowledge/app/rag/load/entry_service.py
+++ project/rag_knowledge/app/rag/load/entry_service.py
@@ -1,7 +1,38 @@
+from pathlib import Path
+
 from ...process.load.agent.state import LoadState
-from ...shared.runtime.logger import step_log
+from ...shared.runtime.logger import logger, step_log
 
 
 @step_log("resolve_input_file")
 def resolve_input_file(state: LoadState) -> LoadState:
+    local_file_path = state.get("local_file_path")
+    if not local_file_path:
+        logger.error(f"local_file_path 参数为空: {local_file_path}")
+        raise ValueError(f"local_file_path 参数为空: {local_file_path}")
+
+    if local_file_path.lower().endswith(".md"):
+        state["md_path"] = local_file_path
+        state["pdf_path"] = None
+        state["is_md_read_enabled"] = True
+        state["is_pdf_read_enabled"] = False
+        logger.info(f"识别到Markdown文件: {local_file_path}")
+
+    elif local_file_path.lower().endswith(".pdf"):
+        state["md_path"] = None
+        state["pdf_path"] = local_file_path
+        state["is_md_read_enabled"] = False
+        state["is_pdf_read_enabled"] = True
+        logger.info(f"识别到PDF文件: {local_file_path}")
+
+    else:
+        logger.error(f"不支持的文件类型(md/pdf): {local_file_path}")
+        raise ValueError(f"不支持的文件类型(md/pdf): {local_file_path}")
+
+    local_file_path_obj = Path(local_file_path)
+    if not local_file_path_obj.is_file():
+        logger.error(f"文件不存在/不是文件: {local_file_path_obj}")
+        raise FileNotFoundError(f"文件不存在/不是文件: {local_file_path_obj}")
+
+    state["file_title"] = local_file_path_obj.stem
     return state
