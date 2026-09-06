132afcfd4217a255951f343d5b266d5114217e78
charlotte <gw1007charlotte@gmail.com>
2026-09-06 10:44:35 +0800

    feat(rag_text2sql): agent node 4

--- project/rag_text2sql/app/agent/nodes/_4_1_filter_table.py
+++ project/rag_text2sql/app/agent/nodes/_4_1_filter_table.py
@@ -1,9 +1,67 @@
+import yaml
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
+from app.agent.prompt_loader import load_prompt
 from app.agent.state import DataAgentState
+from app.core.log import logger
 
 
 async def filter_table(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer({"stage": "过滤表信息"})
+
+    try:
+        # 获取数据
+        query = state["query"]
+        table_infos = state["table_infos"]
+
+        # 定义执行Chain
+        template = await load_prompt("filter_table_info")
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["query", "table_infos"],
+        )
+        output_parser = JsonOutputParser()
+        chain = prompt | llm | output_parser
+
+        result = await chain.ainvoke(
+            {
+                "query": query,
+                "table_infos": yaml.dump(
+                    table_infos,
+                    allow_unicode=True,  # 中文展示
+                    sort_keys=False,  # 不排序
+                ),
+            }
+        )
+
+        """
+        返回的结果:
+        {
+            table1: [column1, column2, ...],
+            table2: [column1, column2, ...]
+        }
+        """
+        logger.info(f"表结构过滤信息\n{result}")
+
+        for table_info in table_infos[:]:  # 浅拷贝: 遍历中有删除操作, 所以要复制一份
+            table_name = table_info["name"]
+            columns = table_info["columns"]
+
+            if table_name not in result:
+                table_infos.remove(table_info)
+
+            for column in columns[:]:  # 浅拷贝: 遍历中有删除操作, 所以要复制一份
+                if column["name"] not in result[table_name]:
+                    columns.remove(column)
+
+        logger.info(f"表结构过滤成功\n{[table['name'] for table in table_infos]}")
+
+        return {"table_infos": table_infos}
+    except Exception as e:
+        logger.error(f"表结构过滤失败\n{e!s}")
+        raise
--- project/rag_text2sql/app/agent/nodes/_4_2_filter_metric.py
+++ project/rag_text2sql/app/agent/nodes/_4_2_filter_metric.py
@@ -1,9 +1,56 @@
+import yaml
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 from langgraph.runtime import Runtime
 
 from app.agent.context import DataAgentContext
+from app.agent.llm import llm
+from app.agent.prompt_loader import load_prompt
 from app.agent.state import DataAgentState
+from app.core.log import logger
 
 
 async def filter_metric(state: DataAgentState, runtime: Runtime[DataAgentContext]):
     writer = runtime.stream_writer
     writer({"stage": "过滤指标信息"})
+
+    try:
+        # 获取数据
+        query = state["query"]
+        metric_infos = state["metric_infos"]
+
+        # 定义执行Chain
+        template = await load_prompt("filter_metric_info")
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["query", "metric_infos"],
+        )
+        output_parser = JsonOutputParser()
+        chain = prompt | llm | output_parser
+
+        result = await chain.ainvoke(
+            {
+                "query": query,
+                "metric_infos": yaml.dump(
+                    metric_infos,
+                    allow_unicode=True,  # 中文展示
+                    sort_keys=False,  # 不排序
+                ),
+            }
+        )
+
+        """
+        返回的结果: [metric1, metric2, ...]
+        """
+        logger.info(f"指标过滤信息\n{result}")
+
+        for metric_info in metric_infos[:]:  # 浅拷贝: 遍历中有删除操作, 所以要复制一份
+            if metric_info["name"] not in result:
+                metric_infos.remove(metric_info)
+
+        logger.info(f"指标过滤成功\n{[metric['name'] for metric in metric_infos]}")
+
+        return {"metric_infos": metric_infos}
+    except Exception as e:
+        logger.error(f"指标过滤失败:\n{e!s}")
+        raise
